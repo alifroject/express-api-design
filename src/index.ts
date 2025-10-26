08cab4b2ed0930053cc941a5529b683574376000
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-10-26 10:07:47 +0700

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import express, { Request, Response } from "express";
 //routes
 import userRoutes from "./routes/user"; 
 import productRoute from "./routes/product"; 
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3005;
 
 app.use(express.json());
+// Allow all origins (for development)
+app.use(cors());
 
 //user
 app.use(userRoutes)
