3847535e38dc2d5fbd6cdad05672e167f97c79a7
Alifnewjourney Jovani safik <furqonseli@gmail.com>
2025-10-31 19:34:36 +0700

    Add cookie http only and csurf for csrf token validations

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,54 @@
-import express, { Request, Response } from "express";
-
-
-//routes
-import userRoutes from "./routes/user"; 
-import productRoute from "./routes/product"; 
-import authRoute from "./routes/auth"
+import express, { Request, Response, NextFunction } from "express";
+import userRoutes from "./routes/user";
+import productRoute from "./routes/product";
+import authRoute from "./routes/auth";
 import cors from "cors";
+import csurf from "csurf";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3005;
 
+app.use(cookieParser());
 app.use(express.json());
-// Allow all origins (for development)
-app.use(cors());
 
+app.use(cors({
+  origin: "*", 
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-CSRF-Token"],
+}));
 
-//auth
-app.use("/api/auth", authRoute)
-//user
-app.use(userRoutes)
-app.use(productRoute)
+//CSRF middleware
+const csrfProtection = csurf({
+  cookie: {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: false, // change to production (HTTPS)
+  },
+});
 
+// Skip CSRF for specific routes
+app.use((req: Request, res: Response, next: NextFunction) => {
+  if (
+    req.path === "/api/csrf-token" ||
+    req.path === "/api/auth/login" ||
+    req.path === "/api/auth/register"
+  ) {
+    return next(); // skip csrf 
+  }
+  return csrfProtection(req, res, next);
+});
+
+// endpoint to take CSRF token
+app.get("/api/csrf-token", (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
+
+// routes
+app.use("/api/auth", authRoute);
+app.use(userRoutes);
+app.use(productRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
